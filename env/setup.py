43fa1a471bcd8c4ccd7cbb704cbc5ba9f395a104
its-me-sweta <swetajha1096@gmail.com>
2024-09-04 22:27:05 +0530

    setup files

---
+++ env/setup.py
@@ -0,0 +1,26 @@
+from typing import List
+from setuptools import find_packages, setup
+HYPEN_E_DOT = '-e . '
+def get_requirements(file_path:str)-> List[str]:
+    '''
+    it will return list of requirement.
+    '''
+
+    requirements = []
+    with open (file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [ req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+   
+    return requirements        
+          
+setup(
+    name = "mlproject",
+    version = '0.0.1',
+    author = 'its_me_sweta',
+    author_email='swetajha1096@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
