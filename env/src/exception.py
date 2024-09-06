2e54ec283ab6f29bd5fe2662eac44173d8a4dd42
its-me-sweta <swetajha1096@gmail.com>
2024-09-06 18:27:23 +0530

    logging and exception

---
+++ env/src/exception.py
@@ -0,0 +1,25 @@
+import sys
+import logging
+
+def error_message_details(error, error_details: sys):
+    _,_, error_tb = error_details.exc_info()
+    file_name = error_tb.tb_frame.f_code.co_filename
+    error_message = "error occured in python script name [{0}] line number[{1}] error message[{2}]".format(
+    file_name, error_tb.tb_lineno, str(error)
+
+    )
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_details=error_details)
+
+    def __str__(self):
+        return self.error_message
+
+
+
+                 
+ 
+    
